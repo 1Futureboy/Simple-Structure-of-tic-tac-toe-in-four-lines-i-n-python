ca056220dc132ec0e1592e5e68771fb0a6458c0c
Aadarsh Raj(Futureboy) <39348021+1Futureboy@users.noreply.github.com>
2019-02-25 00:08:57 +0530

    Add files via upload
    You Want To Create A Tic-Tac-Toe Game And You Have Problem In Structure Of Tic-Tac-Toe Then Relax This Code Can Help You This Is Very Simple 4 Lines On Python Code Can Draw A Structure Of Tic-Tac-Toe Game.

---
+++ tic-tac-toe_structure.py
@@ -0,0 +1,23 @@
+#Code By == Aadarsh Raj(Futureboy)
+
+
+
+
+#Structure Of Tic-Tac-Toe
+
+
+#draw [0],[1],[2],[3],[4],[5],[6], [7],[8],[9]
+draw=[" "," "," "," "," "," "," "," "," "," "]
+
+print("Welcome To Tic-Tac-Toe Structure\n")
+
+#printing a strucuture of tic-tac-toe
+
+print("%s | %s | %s " % (draw[1],draw[2],draw[3])) #draw of liste for fill
+print("-----------")
+print("%s | %s | %s " % (draw[4],draw[5],draw[6])) #draw of liste for fill
+print("-----------")
+print("%s | %s | %s " % (draw[7],draw[8],draw[9])) #draw of liste for fill
+
+print()
+print("Code And Play\n")
